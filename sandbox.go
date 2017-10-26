13aaf60967c6f8d44286ad991bccb9d37068a050
SEKORM\pei_zhou <pei_zhou@sekorm.com>
2017-10-26 17:24:28 +0800

    add

--- sandbox.go
+++ sandbox.go
@@ -5,8 +5,13 @@ import (
 	"time"
 )
 
+func fun1(abc string) {
+	fmt.Println(abc)
+}
+
 func main() {
 	fmt.Println("Welcome to the playground!")
 
 	fmt.Println("The time is", time.Now())
+	fun1("hello")
 }
